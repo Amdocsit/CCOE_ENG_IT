6621072ea843f1a2965ae19b9ab5b51a2b6c096e
Amit Patki <amit.patki@amdocs.com>
2018-08-23 17:08:29 +0530

    spl correct

--- git_test.ps1
+++ git_test.ps1
@@ -3,7 +3,7 @@ if (!(Import-Module -Name VMware.VimAutomation.Core -ErrorAction SilentlyContinu
     # Error out if loading fails  
     Write-Error "`nERROR: Cannot load the VMware Module. Is the PowerCLI installed?"  
  }
- elase{ Write-Output "loaded the VMware COre Module..."}
+ else{ Write-Output "loaded the VMware COre Module..."}
 #endregion
 Connect-VIserver -server "ilvciaas2srv.corp.amdocs.com"
 Get-cluster |ft
